d5b9f746e772bdb5dd9ad7b24256cce81ce1fe9a
inhwanK <inhan1009@naver.com>
2024-11-07 17:38:38 +0900

    initial commit

---
+++ src/main/java/ind/practice/woorinurture/WooriNurtureApplication.java
@@ -0,0 +1,13 @@
+package ind.practice.woorinurture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WooriNurtureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WooriNurtureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ind/practice/woorinurture/WooriNurtureApplicationTests.java
@@ -0,0 +1,13 @@
+package ind.practice.woorinurture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WooriNurtureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
